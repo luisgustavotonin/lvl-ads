3f7d7ce1d2bd846f82bfceae0aa018fd2edc19f8
luisgustavotonin <luisgustavotonin@hotmail.com>
2026-02-21 23:57:34 +0000

    File changes

--- src/pages/DataManagement.jsx
+++ src/pages/DataManagement.jsx
@@ -10,6 +10,7 @@ import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Input } from '@/components/ui/input';
 import { Badge } from '@/components/ui/badge';
+import { Checkbox } from '@/components/ui/checkbox';
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from '@/components/ui/alert-dialog';
 import toast from 'react-hot-toast';
 
