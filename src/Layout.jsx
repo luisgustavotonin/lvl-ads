7f1cc950dd211f9afc61135179b2a5177913a45f
luisgustavotonin <luisgustavotonin@hotmail.com>
2026-02-24 21:12:09 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -38,6 +38,7 @@ const navigation = [
   { name: 'Integrações', href: 'Integrations', icon: Link2 },
   { name: 'Ingestão Meta', href: 'MetaIngest', icon: Zap },
   { name: 'Parâmetros & Alertas', href: 'ParametersAlerts', icon: Bell },
+  { name: 'Dados Brutos', href: 'RawData', icon: Database },
 
   { name: 'Perfis', href: 'Profiles', icon: Shield },
   { name: 'Usuários', href: 'Users', icon: Users },
