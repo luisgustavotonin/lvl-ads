315b36b628cf65addecacc1180fac78d7d53aad6
luisgustavotonin <luisgustavotonin@hotmail.com>
2026-02-27 15:42:45 +0000

    File changes

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -709,7 +709,7 @@ export default function Reports() {
           children={{
             overview: (
               <>
-                <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-2 sm:gap-4">
+                <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-2 sm:gap-4" data-pdf-element>
                   {selectedKPIs.map((kpiId) => {
                     const kpi = ALL_KPIS.find((k) => k.id === kpiId);
                     if (!kpi) return null;
