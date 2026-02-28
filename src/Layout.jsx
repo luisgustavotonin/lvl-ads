91abe180da3255108bfbebf52cdaabd5a3ffc8a1
luisgustavotonin <luisgustavotonin@hotmail.com>
2026-02-28 01:31:49 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { createPageUrl } from './utils';
 import { base44 } from '@/api/base44Client';
-import { useQuery } from '@tanstack/react-query';
 import { cn } from '@/lib/utils';
 import {
   LayoutDashboard,
