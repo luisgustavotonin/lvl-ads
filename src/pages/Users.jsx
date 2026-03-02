98ff2cda62602bc2e05e33b86dfa8b6d598926ac
luisgustavotonin <luisgustavotonin@hotmail.com>
2026-03-02 19:33:26 +0000

    File changes

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -152,6 +152,7 @@ export default function Users() {
           unit_ids: inviteUnitIds,
         });
       }
+      setInviteName('');
       setInviteEmail('');
       setInviteRole('user');
       setInviteProfileId('');
